4d026a70ccde24503d29f7e381c27b6a0892d9dd
Karim Aouadi <kdolphinus@gmail.com>
2018-08-02 01:22:18 +0200

    Updated tutorial 10

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -1,18 +1,28 @@
 <template>
-<div class="edit-smoothie container">
-    <h2 class="edit-smoothie">Edit smoothie {{this.$route.params.smoothie_slug}}</h2>
+<div class="edit-smoothie container" v-if="smoothie">
+    <h2 class="edit-smoothie">  {{smoothie.title}} smoothie</h2>
 
 </div>
     
 </template>
 
 <script>
+import db from "@/firebase/init";
 export default {
     name: 'EditSmoothie',
     data(){
         return{
-
+            smoothie: null
         }
+    },
+    created(){
+        let ref = db.collection('smoothie').where('slug', '==', this.$route.params.smoothie_slug)
+        ref.get().then(snapshot =>{
+            snapshot.forEach(doc => {
+                this.smoothie = doc.data()
+                this.smoothie.id = doc.id
+            });
+        })
     }
 
 }
